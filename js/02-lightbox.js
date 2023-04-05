3e26a23080236f2fbc2ccfb591705d4a52b46c3a
natske77 <maksnatske@gmail.com>
2023-04-05 03:22:07 +0300

    ДЗ №7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -6,3 +6,21 @@ console.log(galleryItems);
 const photoGallery = document.querySelector('.gallery');
 photoGallery.innerHTML = markupGalleryItems(galleryItems);
 
+const gallery = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
+});
+
+function markupGalleryItems(items) {
+    return items.map(item =>
+        `<li class="galery__item">
+        <a class="gallery__link" href="${item.original}">
+        <img class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"
+        />
+        </a>
+        </li>`
+    ).join('');
+};
